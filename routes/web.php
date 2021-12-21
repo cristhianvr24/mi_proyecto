419f7a60036eaace33495784081f948f64ff1572
unknown <cristhian24vr@gmail.com>
2021-12-21 16:03:38 -0400

    noveno

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,4 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', HomeController::class);
 
-route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
-
-Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
-
-Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
-
-Route::get ('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
-
-Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
-
-Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
-
-Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+Route::resource('cursos', CursoController::class);
